be71e2de4dc568ef72b4b908c2385c3c31ddfb98
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-12-22 12:29:08 +0100

    Implement tests for available buns and ingredients
    Added unit tests for available buns and ingredients.

--- database.py
+++ database.py
@@ -31,3 +31,32 @@ class Database:
 
     def available_ingredients(self) -> List[Ingredient]:
         return self.ingredients
+
+
+
+
+
+
+import pytest
+from praktikum.database import Database
+
+
+class TestDatabase:
+    def test_available_buns(self):
+        database = Database()
+        buns = database.available_buns()
+        assert len(buns) == 3
+        assert buns[0].name == "black bun"
+        assert buns[1].name == "white bun"
+        assert buns[2].name == "red bun"
+
+    def test_available_ingredients(self):
+        database = Database()
+        ingredients = database.available_ingredients()
+        assert len(ingredients) == 6
+        assert ingredients[0].name == "hot sauce"
+        assert ingredients[1].name == "sour cream"
+        assert ingredients[2].name == "chili sauce"
+        assert ingredients[3].name == "cutlet"
+        assert ingredients[4].name == "dinosaur"
+        assert ingredients[5].name == "sausage"
