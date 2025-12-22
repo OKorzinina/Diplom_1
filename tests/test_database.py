9efdef40ec7557e5e31f3247c67356539b51a71c
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-12-22 13:08:31 +0100

    Implement initial tests for Database class
    Add initial tests for database counts of buns and ingredients.

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,1 +1,8 @@
+from praktikum.database import Database
+
+class TestDatabase:
+    def test_database_initial_counts(self):
+        db = Database()
+        assert len(db.available_buns()) == 3
+        assert len(db.available_ingredients()) == 6
 
