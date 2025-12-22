3d4b6a2a5d3d1bd2a9bc10af5e08f3c499813a49
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-12-22 12:42:13 +0100

    Refactor TestBun class and add parameterized tests

--- bun.py
+++ bun.py
@@ -18,16 +18,32 @@
 #from praktikum.bun import Bun
 
 
+#class TestBun:
+    #def test_bun_creation(self):
+        #bun = Bun("Wheat Bun", 100.0)
+       # assert bun.name == "Wheat Bun"
+       # assert bun.price == 100.0
+
+    #def test_get_name(self):
+        #bun = Bun("Rye Bun", 120.0)
+        #assert bun.get_name() == "Rye Bun"
+
+    #def test_get_price(self):
+        #bun = Bun("Sesame Bun", 80.0)
+        #assert bun.get_price() == 80.0
+
+
+import pytest
+from praktikum.bun import Bun
+
 class TestBun:
-    def test_bun_creation(self):
-        bun = Bun("Wheat Bun", 100.0)
-        assert bun.name == "Wheat Bun"
-        assert bun.price == 100.0
-
-    def test_get_name(self):
-        bun = Bun("Rye Bun", 120.0)
-        assert bun.get_name() == "Rye Bun"
-
-    def test_get_price(self):
-        bun = Bun("Sesame Bun", 80.0)
-        assert bun.get_price() == 80.0
+    @pytest.mark.parametrize("name, price", [
+        ("black bun", 100),
+        ("white bun", 200.5),
+        ("", 0),
+        ("special bun with long name", -10)
+    ])
+    def test_bun_initialization_and_getters(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_name() == name
+        assert bun.get_price() == price
